5dd4b804bef929221200f2e5f4e8277b5968da12
mb301878 <mb301878@gmail.com>
2022-09-08 12:39:35 +0200

    feature: simple liquibase app, copied from https://github.com/bykowski/springboot-liquibase

---
+++ liquibase-basic/src/test/java/mb301878/MainTest.java
@@ -0,0 +1,13 @@
+package mb301878;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
